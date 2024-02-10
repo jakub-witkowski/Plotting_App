4b501fe51d193008659bc7d487dc190dd51a327d
jakub.witkowski <j.witkowski@poczta.onet.pl>
2024-02-10 20:11:21 +0100

    further unit tests

--- Tests/tst_datasettests.cpp
+++ Tests/tst_datasettests.cpp
@@ -3,32 +3,41 @@
 #include <gmock/gmock-matchers.h>
 #include <gtest/gtest.h>
 
+TData dataset("1050.csv");
+
 TEST(PlottingAppTests, Load_Input_Raw_Data)
 {
-    TData dataset("1050.csv");
+    // TData dataset("1050.csv");
     dataset.load_input();
     ASSERT_TRUE(dataset.get_raw_data_size() > 0);
 }
 
 TEST(PlottingAppTests, Load_Input_Ages)
 {
-    TData dataset("1050.csv");
+    // TData dataset("1050.csv");
     dataset.load_input();
     ASSERT_TRUE(dataset.get_ages_vector_size() > 0);
 }
 
 TEST(PlottingAppTests, Load_Input_Depths)
 {
-    TData dataset("1050.csv");
+    // TData dataset("1050.csv");
     dataset.load_input();
     ASSERT_TRUE(dataset.get_depths_vector_size() > 0);
 }
 
 TEST(PlottingAppTests, Compare_Ages_And_Depth_Vectors_Length)
 {
-    TData dataset("1050.csv");
+    // TData dataset("1050.csv");
     dataset.load_input();
     ASSERT_EQ(dataset.get_depths_vector_size(), dataset.get_ages_vector_size()) << "Depths and Ages vectors are of unequal length.";
+}
+
+TEST(PlottingAppTests, Test_Segment_Index_Vector)
+{
+    dataset.load_input();
+    dataset.find_hiatus();
+    ASSERT_TRUE(dataset.get_segment_indexes_size() > 0);
 }
 
 int main(int argc, char** argv)
